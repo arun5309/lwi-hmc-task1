7b1ff2efb981f0053ecd515c230a5b19badc4212
Im-arun <arunachalaeshwaran@tutanota.com>
2020-06-11 14:18:27 +0530

    Added cron for updates

--- terraform_code/install_apache.sh
+++ terraform_code/install_apache.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-sudo yum install git httpd -y
+sudo yum install httpd -y
 sudo mkfs.ext4 /dev/xvdh
 sudo mount /dev/xvdh /var/www/html
-git clone https://github.com/arun5309/lwi-hmc-task1.git
-sudo cp -r lwi-hmc-task1/html /var/www
+# git clone https://github.com/arun5309/lwi-hmc-task1.git
+(sudo crontab -l ; sudo echo "* * * * * (sudo wget \"https://raw.githubusercontent.com/arun5309/lwi-hmc-task1/master/html/index.html\" -O /var/www/html/index.html)") | sudo crontab - 
+# sudo cp -r lwi-hmc-task1/html /var/www
 sudo systemctl --now enable httpd
 
